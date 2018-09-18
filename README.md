2c47234b4f6a72dd7bfa7edc254873f309e6463b
Judi Portwood <yipnm123@gmail.com>
2018-09-17 22:33:03 -0400

    Added algorithm solution for conversion C to F

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-FCC Portfolio
+#FCC Portfolio
 
-This is a log of all my code from FCC
+This is a log of some of my code from FCC. I'm keeping a log of mostly my JavaScript code in case I need to reference it later.
+
+##Author
+
+Judi Portwood
--- algorithms.js
+++ algorithms.js
@@ -1,1 +1,8 @@
-// algorithm solutions for challenges in FCC
+// algorithm solutions for basic algorithms scripting challenges in FCC
+
+
+// Convert Celsius to Fahrenheit
+function convertToF(celsius) {
+    let fahrenheit = celsius * (9 / 5) + 32;
+    return fahrenheit;
+}

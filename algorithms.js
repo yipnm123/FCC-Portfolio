171f8d0b6e0ba37746461c6893883fc49b751f0e
Judi Portwood <yipnm123@gmail.com>
2018-09-20 18:47:39 -0400

    Factorialize algorithm solution added.

--- algorithms.js
+++ algorithms.js
@@ -12,3 +12,21 @@ function reverseString(str) {
     let newStr = str.split('').reverse().join('');
     return newStr;
 }
+
+// Factorialize a Number. Only run positive numbers and if 0, return 1.
+function factorialize(num) {
+    let Solution = 1;
+    // if number is less than 0
+    if(num < 0){
+      return "Error";
+    // if number is greater than 0
+    } else if(num > 0) {
+      for(let i = 1; i <= num; i++) {
+        Solution *= i;
+      }
+      return Solution;
+      // if number is 0
+    } else{
+    return Solution;
+    }
+  } 
